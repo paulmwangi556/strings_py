f971f519c3ae73e416802970b39ab9c407e4c5ff
paulmwangi556 <pmwas8294@gmail.com>
2021-02-24 16:54:30 +0300

    sixth commit

---
+++ pythonstrings.py
@@ -0,0 +1,55 @@
+#python operators
+
+
+#strings in python
+a = 5
+b = 10
+c = a+b
+a =+ b
+print(c)
+print(a)
+#multiplying strings in python
+print("3" *4)
+print("john" + "was" + ("here" * 3))
+#slicing in python
+a = "hello world"
+b = "paul"
+print(a[2:5])
+print(a[3])
+print(b[2])
+print(b[-1])
+#out of range error message
+s1 = "Ann Dawson"
+print(s1[0:1],s1[5:7])
+print("line 24:",s1[16:9])
+#slicing using the [ operator]
+s = "hello world"
+s[3:7]
+s[2:4]
+s[1:6]
+s[5:8]
+s[-2:5]
+print(s[3:7])
+print(s[2:4])
+print(s[1:6])
+print(s[5:8])
+print(s[-2:5])
+#finding the length of string using len
+s1 = "Paul"
+s2 = "Mwangi"
+s3 = ""
+s4 = "my string length output"
+print(len(s1), end=" ")
+print(len(s2), end=" ")
+print(len(s4), end=" ")
+print(len(s3))
+#print integer within a string
+x = 20
+y = 75
+print('sum of %d and %d is %d' % (x, y, x + y))
+
+
+
+
+
+
